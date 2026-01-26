687891dae2249428594e591749019c025cae8df7
Trevis Elser <trevis@flipstone.com>
2026-01-26 15:59:35 -0500

    Update examples
    Some improvements to showing package qualified imports, encapsulation,
    haddock checks, postpositive qualification, and matching (or not)
    module patterns.

---
+++ examples/src/PetStore/Food.hs
@@ -0,0 +1,24 @@
+{- |
+Module      : PetStore.Food
+Description : An example module
+License     :
+Maintainer  : sample@email.com
+
+Here is a longer description of this module, containing some
+commentary with @some markup@.
+-}
+
+module PetStore.Food where
+
+{- | getFood does a thing
+
+-}
+getFood :: IO ()
+getFood = do
+  putStrLn "Not Yet Implemented"
+  pure ()
+
+setFood :: IO ()
+setFood = do
+  putStrLn "Not Yet Implemented"
+  pure ()
---
+++ examples/src/Vendor.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE PackageImports #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+{- |
+Module      : Vendor
+Description : An example module
+License     :
+Maintainer  : sample@email.com
+
+Here is a longer description of this module, containing some
+commentary with @some markup@.
+-}
+
+module Vendor where
+
+import Control.Arrow qualified as A
+import Control.Monad as M
+import Data.Foldable as F
+import Data.Functor as F
+import Data.Monoid as M
+import Data.Text
+import Prelude qualified
+import System.IO.Unsafe (unsafePerformIO)
+import qualified "unliftio" UnliftIO as Foo
+import qualified UnliftIO.Async as Foo
+
+-- Note this is not haddock
+bar :: Prelude.IO ()
+bar = Prelude.undefined
+
+{-| This is a
+longer
+form
+haddock
+
+@since 6.7.8.9
+-}
+baz = Prelude.undefined
+
+-- | This is some haddock
+foo :: Prelude.IO ()
+foo = unsafePerformIO Prelude.undefined
+
+data Bar =
+  A_BAR -- ^ Thing
+  | B_BAR -- ^ B
+
+class MyShow a where
+  myShow :: a -> Prelude.String
